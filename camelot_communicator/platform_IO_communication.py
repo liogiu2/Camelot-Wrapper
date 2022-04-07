b8cf49207889f8625059ebafaea239d40da7315a
Giulio Mori <mori.giulio.94@gmail.com>
2022-04-07 16:56:02 +0200

    Update platform_IO_communication.py

--- camelot_communicator/platform_IO_communication.py
+++ camelot_communicator/platform_IO_communication.py
@@ -1,6 +1,9 @@
 from utilities import singleton
 import requests
 import json
+import time
+import threading
+import queue
 import debugpy
 
 
@@ -13,6 +16,21 @@ class PlatformIOCommunication:
     def __init__(self):
         self.base_link  = "http://127.0.0.1:8080"
         self.__online = self._is_platform_online()
+        self.__message_queue = queue.Queue()
+        self.__input_thread = threading.Thread(target=self.__receive_message_thread, args=(self.__message_queue), daemon=True)
+        self.initial_message_link = ""
+        self.receive_message_link = ""
+        self.send_message_link = ""
+
+    def __receive_message_thread(self, message_queue: queue.Queue):
+        """
+        This method is used to create a thread that continuosly makes request to the platform to receive a new message when available.
+        """
+        while self.__online:
+            message = self._receive_message()
+            if message != "":
+                message_queue.put(message)
+            time.sleep(0.2)
 
     def send_message(self, message):
         """
@@ -24,10 +42,24 @@ class PlatformIOCommunication:
             The message to be sent.
         """
         if self.__online:
-            response = requests.post(self.base_link + "/add_message", data = json.dumps({'text':message}))
+            response = requests.post(self.base_link + self.send_message_link, data = json.dumps({'text':message}))
             pass
+    
+    def get_received_message(self):
+        """
+        This method is used to get the received message from the platform.
 
-    def receive_message(self) -> str:
+        Returns
+        -------
+        str
+            The received message. "" if no message is available.
+        """
+        try:
+            return self.__message_queue.get_nowait()
+        except queue.Empty:
+            return ""
+
+    def _receive_message(self) -> str:
         """
         This method is used to receive a message from the platform.
 
@@ -37,7 +69,7 @@ class PlatformIOCommunication:
             The message received from the platform.
         """
         if self.__online:
-            response = requests.get(self.base_link + "/get_em_message")
+            response = requests.get(self.base_link + self.receive_message_link)
             return response.json()['text']
         return ""
     
@@ -63,12 +95,16 @@ class PlatformIOCommunication:
         bool
             True if the API is online, False otherwise.
         """
+        t = threading.Timer(10.0, self._is_platform_online)
+        t.start()
         try:
             response = requests.head(self.base_link + "/")
             if response.status_code == 200:
                 return True
             else:
+                t.cancel()
                 return False
         except:
+            t.cancel()
             return False
 
